d2005bfc299c1cf2b60ad18fbed5e5ea396be238
Vladislav Golosov <v.golosoff@gmail.com>
2021-10-26 23:37:31 +0300

    Interface build

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Владислав Голосов on 26.10.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var forgotUserName: UIButton!
+    @IBOutlet var forgotPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func forgotButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
