01552ad04b4f04927777fe5c868f28ba236f5eb1
VladGolosoff <v.golosoff@gmail.com>
2021-10-27 16:32:19 +0300

    Work build

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -10,19 +10,41 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var loginText: UITextField!
     @IBOutlet var passwordText: UITextField!
-    @IBOutlet var forgotUserName: UIButton!
-    @IBOutlet var forgotPassword: UIButton!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-    @IBAction func loginTapped(_ sender: UIButton) {
-        if loginText.text == "User" && passwordText.text == "Password" {
-        performSegue(withIdentifier: "loginSegue", sender: nil)
-        }
-        }
     
+    private func showAlert(title: String, messege: String) {
+        let alert = UIAlertController(title: title, message: messege, preferredStyle: .alert)
+        let allCorrect = UIAlertAction(title: "OK", style: .default) {_ in
+            self.passwordText.text = ""
+        }
+        alert.addAction(allCorrect)
+        present(alert , animated: true)
+    }
+    @IBAction func forgotNameTap(_ sender: UIButton) {
+        showAlert(title: "Oops!", messege: "Your name is Username")
+    }
+    @IBAction func forgotPassButton(_ sender: UIButton) {
+        showAlert(title: "Oops!", messege: "Your password is Password")
+    }
+    
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        if loginText.text == "Username" && passwordText.text == "Password" {
+            performSegue(withIdentifier: "loginSegue", sender: nil)
+        } else {
+            showAlert(title: "Invalid username or password!", messege: "Please, enter correct username or password")
+        }
+    }
+
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationView = segue.destination as? LoginViewController else {return}
         destinationView.login = loginText.text
