5be12574630f5f2d1a9e2bf480004d4284f10dde
VladGolosoff <v.golosoff@gmail.com>
2021-10-30 23:00:34 +0300

    Hierarchical

--- LoginApp/ViewControllers/LoginViewController.swift
+++ LoginApp/ViewControllers/LoginViewController.swift
@@ -11,9 +11,9 @@ class LoginViewController: UIViewController {
     var login: String?
     @IBOutlet var welcomeLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         guard let login = login else { return }
         welcomeLabel.text = "Hello, \(login)"
     }
